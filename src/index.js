7a4968d5469f33d9269ed6a939a311c463951623
Andy Jagoe <andy@jagoe.com>
2022-06-17 21:35:11 -0700

    add logging support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import axios from 'axios'
 import { exec } from 'child_process'
+import dotenv  from "dotenv"
+import { client, v1 } from '@datadog/datadog-api-client'
+
+dotenv.config()
+const configuration = client.createConfiguration()
+const apiInstance = new v1.EventsApi(configuration)
+
 
 const SERVER = 'http://localhost:8000'
 const CONNECTION_ID = '47393f4b-2d3e-452f-9d2a-f3e01e1240b2'
@@ -15,6 +22,8 @@ function shutdown(callback){
 
 async function main() {
     let serverReady = false
+
+    console.log(`Test: ${process.env.DATADOG_KEY}`)
 
     while (!serverReady) {
         console.log(`Checking if Airbyte server is ready: ${Date.now()}`)
@@ -41,7 +50,27 @@ async function main() {
         .post(`${SERVER}/api/v1/jobs/get`, { id: jobId })
         .catch(err => console.log(err.message))
     
-        if (status?.data?.job?.status !== 'running') jobRunning = false
+        if (status?.data?.job?.status !== 'running') {
+            console.log(status?.data?.attempts[0]?.attempt)
+            jobRunning = false
+
+            const params = {
+                body: {
+                  title: "Airbyte Sync Ran Successfully",
+                  text: JSON.stringify(status?.data?.attempts[0]?.attempt),
+                },
+              }
+        
+            apiInstance
+            .createEvent(params)
+            .then((data) => {
+                console.log(
+                "API called successfully. Returned data: " + JSON.stringify(data)
+                );
+            })
+            .catch((error) => console.error(error))
+        
+        }
         await sleep(1000)    
     }
 
